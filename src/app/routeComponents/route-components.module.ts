8a4079b3cc994970771edac42f68e520b5bac468
jamesbrobb <james@jamesrobb.com>
2021-07-21 14:29:25 +0100

    udpated routes to include parent routes to mirror menu

--- src/app/routeComponents/route-components.module.ts
+++ src/app/routeComponents/route-components.module.ts
@@ -5,14 +5,24 @@ import {GridLayoutRouteComponent} from "./components/grid-layout/grid-layout-rou
 import {FlexGridRouteComponent} from "./components/flex-grid/flex-grid-route.component";
 import {ResponsiveContainerRouteComponent} from "./components/responsive-container/responsive-container-route.component";
 import {RouterModule, Routes} from "@angular/router";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
-  { path: 'grid', component: GridLayoutRouteComponent },
-  { path: 'flex-grid', component: FlexGridRouteComponent },
-  { path: 'responsive-container', component: ResponsiveContainerRouteComponent },
+  {
+    path: 'components',
+    children: [{
+      path: 'layout',
+      children: [
+        {path: 'grid', component: GridLayoutRouteComponent},
+        {path: 'flex-grid', component: FlexGridRouteComponent},
+        {path: 'responsive-container', component: ResponsiveContainerRouteComponent}
+      ]
+    }]
+  }
 ]
 
 export const COMPONENTS = [
+  PageNotFoundComponent,
   GridLayoutRouteComponent,
   FlexGridRouteComponent,
   ResponsiveContainerRouteComponent
