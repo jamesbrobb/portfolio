819dd419220d4f4fb9b6731fdd3e608986df54a8
jamesbrobb <james@jamesrobb.com>
2021-07-21 14:26:41 +0100

    added route change handler to side menu to ensure correct tree node is open

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnDestroy} from '@angular/core';
 import {NestedTreeControl} from '@angular/cdk/tree';
 import {MatTreeNestedDataSource} from '@angular/material/tree';
+import {NavigationEnd, Router, RouterEvent} from "@angular/router";
+import {filter} from "rxjs/operators";
 
 
 export interface MenuItemNode {
@@ -11,17 +13,19 @@ export interface MenuItemNode {
 
 const MENU_DATA_PROVIDER: MenuItemNode[] = [{
   name: 'Components',
+  path: 'components',
   children: [{
     name: 'Layout',
+    path: 'layout',
     children: [{
       name: 'Grid',
-      path: 'grid'
+      path: 'components/layout/grid'
     }, {
       name: 'Flex Grid',
-      path: 'flex-grid'
+      path: 'components/layout/flex-grid'
     },{
       name: 'Responsive',
-      path: 'responsive-container'
+      path: 'components/layout/responsive-container'
     }]
   }]
 }];
@@ -33,14 +37,56 @@ const MENU_DATA_PROVIDER: MenuItemNode[] = [{
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss']
 })
-export class SideMenuComponent {
+export class SideMenuComponent implements OnDestroy {
+
+  private _router: Router;
 
   treeControl = new NestedTreeControl<MenuItemNode>(node => node.children);
   dataSource = new MatTreeNestedDataSource<MenuItemNode>();
 
-  constructor() {
+  constructor(router: Router) {
+
+    this._router = router;
+    this._router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(this._handleRouteChange);
+
     this.dataSource.data = MENU_DATA_PROVIDER;
+    this.treeControl.dataNodes = MENU_DATA_PROVIDER;
+  }
+
+  ngOnDestroy(): void {
+
   }
 
   hasChild = (_: number, node: MenuItemNode) => !!node.children && node.children.length > 0;
+
+  private _handleRouteChange = (event: any): void => {
+
+    const rEvt: RouterEvent = event as RouterEvent;
+    const frags: string[] = rEvt.url.split('/')
+                                    .filter(value => !!value);
+
+    let node: MenuItemNode | undefined;
+    let nodes: MenuItemNode[] = this.treeControl.dataNodes;
+
+    frags.map((frag: string) => {
+
+      node = nodes.find((value: MenuItemNode) => value.path === frag);
+
+      if(!node) {
+        return;
+      }
+
+      if(!this.treeControl.isExpanded(node)){
+        this.treeControl.expand(node);
+      }
+
+      if(!node.children) {
+        return;
+      }
+
+      nodes = node.children;
+    });
+  }
 }
