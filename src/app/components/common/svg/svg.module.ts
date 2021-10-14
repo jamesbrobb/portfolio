0c2ec7082de7d30d6ec585d97b1ed5d02fd1251f
jamesbrobb <james@jamesrobb.com>
2021-10-14 12:21:23 +0100

    deleted rounded corner svg component

--- src/app/components/common/svg/svg.module.ts
+++ src/app/components/common/svg/svg.module.ts
@@ -1,21 +1,18 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import {DomSanitizer} from "@angular/platform-browser";
+import {MatIconModule, MatIconRegistry} from "@angular/material/icon";
 
 import { SvgComponent } from './svg/svg.component';
 import { SvgRegistry } from './registry/svg-registry';
-
 import { SvgsConfigInjectionToken } from './svg.config.injection-token';
-import { RoundedCornerSvgComponent } from './rounded-corner-svg/rounded-corner-svg.component';
-import { CommonModule } from '@angular/common';
-import {MatIconModule, MatIconRegistry} from "@angular/material/icon";
 import {SvgsConfig} from "./svg.config";
-import {DomSanitizer} from "@angular/platform-browser";
 
 
 
 const components = [
-    SvgComponent,
-    RoundedCornerSvgComponent
+    SvgComponent
 ];
 
 export const svgRegistryFactory = (materialIconRegistry: MatIconRegistry, sanitizer: DomSanitizer, config:SvgsConfig) => {
