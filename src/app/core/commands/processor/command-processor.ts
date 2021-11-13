d3b4fd2a264550d29b578d7388bd5f26fe637f94
jamesbrobb <james@jamesrobb.com>
2021-11-13 00:21:00 +0000

    updated processor to use command group instead of array and to infer types from group, and added support for forwarding extra args to commands

--- src/app/core/commands/processor/command-processor.ts
+++ src/app/core/commands/processor/command-processor.ts
@@ -1,7 +1,14 @@
-import {Conditional, EqualsNever, TypeEqualsType} from "../../../../types";
+import {Conditional, EqualsNever} from "../../../../types";
 import {Observable, of} from "rxjs";
 import {mergeMap} from "rxjs/operators";
 import {Command, ObservableCommand} from "../command";
+import {
+    CommandGroupTypeTemplate,
+    GetCommandGroupBypassType,
+    GetCommandGroupExtraArgsType,
+    GetCommandGroupIOType
+} from "../group/command-group";
+
 
 
 export type CommandProcessorBypassCondition<A, B> = (input: A | B) => input is B;
@@ -13,25 +20,34 @@ export type CommandProcessorBypassConditionArgType<A, B> =
         [bypassCondition: CommandProcessorBypassCondition<A, B>]
     >
 
-type CommandsType<A, B> = (Command<A, A | B> | ObservableCommand<A, A | B>)
+type CommandsType<IO, BY, Xtra extends unknown[]> = (Command<IO, IO | BY, Xtra> | ObservableCommand<IO, IO | BY, Xtra>)
+
+
+
 
 
 export class CommandProcessor {
 
-    execute<IOType, BypassType = never>(
-
+    execute<
+        GroupType extends CommandGroupTypeTemplate,
+        IOType = GetCommandGroupIOType<GroupType>,
+        BypassType = GetCommandGroupBypassType<GroupType>,
+        ExtraArgsType extends unknown[] = GetCommandGroupExtraArgsType<GroupType>
+    >(
+        commandGroup: GroupType,
         input: IOType,
-        commands: CommandsType<IOType, IOType | BypassType>[],
+        extraArgs: ExtraArgsType,
         ...args: CommandProcessorBypassConditionArgType<IOType, BypassType>
 
     ): Observable<IOType | BypassType> {
 
         const initialValue: Observable<IOType> = input instanceof Observable ? input : of(input),
+            commands: ReadonlyArray<CommandsType<IOType, IOType | BypassType, ExtraArgsType>> = commandGroup.getCommands() as ReadonlyArray<CommandsType<IOType, IOType | BypassType, ExtraArgsType>>,
             bypassCondition = args[0];
 
         return commands.reduce(
 
-            (observable: Observable<IOType | BypassType>, command: CommandsType<IOType, IOType | BypassType>): Observable<IOType | BypassType> => {
+            (observable: Observable<IOType | BypassType>, command: CommandsType<IOType, IOType | BypassType, ExtraArgsType>): Observable<IOType | BypassType> => {
 
                 return observable.pipe(
                     mergeMap(
@@ -41,7 +57,7 @@ export class CommandProcessor {
                                 return of(inpt);
                             }
 
-                            const result = command.execute(inpt as IOType);
+                            const result = command.execute(inpt as IOType, ...extraArgs);
 
                             return result instanceof Observable ? result : of(result);
                         })
